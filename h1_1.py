dca8ca84ef4cfe0c75746068746b3b78e2413fd9
Jelte Dirks <j.dirks@outlook.com>
2022-12-07 13:53:47 +0100

    some comments and reworking the printing of results
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- h1_1.py
+++ h1_1.py
@@ -1,13 +1,14 @@
 import numpy as np;
 import math;
 
+# the original function we are trying to minimize using gradient descent
+# f(x) = x_1 ^ 4 + 4x_1x_2 + 2x_2 + (x_2 ^ 2 ) / 2
 def f(x):
     x1 = x[0];
     x2 = x[1];
     return pow(x1, 4) + 4 * x1 * x2 + 2 * x2 + (pow(x2,2) / 2);
 
-# Returns gradient at point x = [x1, x2] with function:
-# f(x) = x1^4 + 4 * x1 * x2 + 2 * x2 + x2^2 / 2
+# returns gradient of f(x) at point x = [x1, x2] 
 # gradient of f(x) is (4 * x1 ^ 3 + 4 * x4 , 4 * x1 + 2 + x2)
 def grad_f(x):
     x1 = x[0];
@@ -16,52 +17,59 @@ def grad_f(x):
     x2_new = 4 *  x1 + 2 + x2;
     return np.array([x1_new, x2_new]);
 
-def gradient_descent(f, grad_f, eta, x0, max_iter=100):
+# gradient descent algorithm implementation
+def gradient_descent(f, grad_f, eta, x0, max_iter=100, show_steps = False):
     xt = x0;
-    print("first xt=" + str(xt));
+    if (show_steps):
+        print("first xt=" + str(xt));
     for i in range(1, max_iter + 1):
         eta_t = eta(i);
         grad_t = grad_f(xt);
         xt = xt - eta_t * grad_t;
-        print("step=" + str(i) + " eta=" + str(eta_t) + " grad_f(xt)=" + str(grad_t) + " newxt=" + str(xt));
-    return xt;
+        if (show_steps):
+            print("step=" + str(i) + " eta=" + str(eta_t) + " grad_f(xt)=" + str(grad_t) + " newxt=" + str(xt));
+    xresult = f(xt);
+    print("f(x_" + str(max_iter) + ")=" + str(xresult));
+    return xresult;
 
+# constant eta function constructor
 def eta_const(c = 0.1):
     def eta(t):
         return c;
     return eta;
 
+# c / sqrt(t + 1) eta function constructor
 def eta_sqrt(c = 0.1):
     def eta(t):
         return c / math.sqrt(t + 1);
     return eta;
 
+# milestone based eta function constructor
 def eta_multistep(milestones, eta_init = 0.1, c = 0.1):
     def eta(t):
-        # if it passed all milestones, easy calculation
-        if (t >= milestones[-1]):
-            return pow(c, milestones.size) * eta_init;
-        i = 0;
         p = 0;
-        # if it hasn't passed all milestones, calculate the correct scalar
-        while (t >= milestones[i]):
-            p = p + 1;
-            i = i + 1;
+        if (t >= milestones[-1]):
+            # if it passed all milestones, easy calculation
+            p = milestones.size;
+        else:
+            # if it hasn't passed all milestones, calculate the correct scalar
+            i = 0;
+            while (t >= milestones[i]):
+                p = p + 1;
+                i = i + 1;
         return pow(c, p) * eta_init;
     return eta;
-        
-# Initial point (0,0)
+
+# Initial point (1,1)
 x00 = np.array([1,1]);
 
+print("constant eta with c = 0.01");
 x100 = gradient_descent(f, grad_f, eta_const(c = 0.01), x00, 100);
-print("f(x_100) using constant step size 0.01 = " + str(f(x100)));
-print("\n" * 2);
 
+print("\neta calculation c / sqrt(t + 1)");
 x100 = gradient_descent(f, grad_f, eta_sqrt(c = 0.1), x00, 100);
-print("f(x_100) using c/sqrt(t+1) step size with c = 0.1 = " + str(f(x100)));
-print("\n" * 2);
 
+print("\nmilestone based eta");
 milestones = np.array([10, 60, 90]);
 x100 = gradient_descent(f, grad_f, eta_multistep(milestones, eta_init = 0.1, c = 0.5), x00, 100);
-print("f(x_100) using milestones="+str(milestones)+" eta policy = " + str(f(x100)));
 
